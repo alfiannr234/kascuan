bb8c239be6fdbf9e9621a47a65714f570c04a9e4
Alfiannt234 <alfiancimot234@gmail.com>
2026-06-17 22:31:21 +0700

    benakke

--- server/db.js
+++ server/db.js
@@ -4,8 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { Pool } = pg;
 
+
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Tes koneksi
