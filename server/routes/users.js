1863c01b7e77bc7f8e1efb500ed9541f24ce4d61
Alfiannt234 <alfiancimot234@gmail.com>
2026-06-18 10:17:51 +0700

    Final Projek 2

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,6 +12,10 @@ const router = express.Router();
 const FRONTEND_URL = 'https://kascuan-six.vercel.app';
 
 const transporter = nodemailer.createTransport({
+    // KITA TULIS MATI DI SINI AGAR TIDAK TERSESAT KE LOCALHOST
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
