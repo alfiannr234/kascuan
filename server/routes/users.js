5bb25e399a71e10a955cd6c15856fafd6a1ca8ef
Alfiannt234 <alfiancimot234@gmail.com>
2026-06-18 10:50:30 +0700

    Final Projek 3

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,8 +13,9 @@ const FRONTEND_URL = 'https://kascuan-six.vercel.app';
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
